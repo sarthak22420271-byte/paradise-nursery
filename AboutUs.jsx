5bc9a9a06426c704c67de61d008bbb4b7aa12ff3
sarthak22420271-byte <sarthak.22420271@viit.ac.in>
2026-04-09 23:18:09 +0530

    Add files via upload

---
+++ AboutUs.jsx
@@ -0,0 +1,13 @@
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Paradise Nursery is your one-stop shop for beautiful indoor plants.
+        We provide high-quality plants to brighten your home.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
