06a0303a81e2df08bdbcf5c0a8d78c90a07af016
sarthak22420271-byte <sarthak.22420271@viit.ac.in>
2026-04-09 23:20:35 +0530

    Add files via upload

---
+++ CartSlice.jsx
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addToCart: (state, action) => {
+      const item = state.items.find(i => i.id === action.payload.id);
+      if (item) {
+        item.quantity++;
+      } else {
+        state.items.push({ ...action.payload, quantity: 1 });
+      }
+    },
+    removeFromCart: (state, action) => {
+      state.items = state.items.filter(i => i.id !== action.payload);
+    },
+    increaseQty: (state, action) => {
+      const item = state.items.find(i => i.id === action.payload);
+      item.quantity++;
+    },
+    decreaseQty: (state, action) => {
+      const item = state.items.find(i => i.id === action.payload);
+      if (item.quantity > 1) item.quantity--;
+    }
+  }
+});
+
+export const { addToCart, removeFromCart, increaseQty, decreaseQty } = cartSlice.actions;
+export default cartSlice.reducer;
